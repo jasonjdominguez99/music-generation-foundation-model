d850c589b819253eee8e25229cdeac3fd8360333
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-05-18 16:22:20 +0100

    Created files to start implementing music foundation model replication study

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+#
+# author: Jason Dominguez
+# date: 2022-05-18
+#
+# Main source code file for implementing the
+# music generation foundation model
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ music_foundation_model.py
@@ -0,0 +1,7 @@
+# music_foundation_model.py
+#
+# author: Jason Dominguez
+# date: 2022-05-18
+#
+# Source code for the music generation
+# model implementation
